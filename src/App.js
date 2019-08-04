1e9d89b3856cf48404c622ffcef93a99e93d40f8
Caroline Marsden <car.marsden@gmail.com>
2019-08-04 18:04:01 -0400

    App starting point

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This will be my 3aDay app.
         </p>
         <a
           className="App-link"
