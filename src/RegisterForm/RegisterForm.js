6b76374fa24b60436f175e6d30bed1083086c39f
Caroline Marsden <car.marsden@gmail.com>
2019-08-08 23:34:32 -0400

    Add account registration form validation

--- src/RegisterForm/RegisterForm.js
+++ src/RegisterForm/RegisterForm.js
@@ -2,6 +2,21 @@ import React from 'react';
 import './RegisterForm.css';
 
 class RegisterForm extends React.Component {
+    state = {
+        username: '',
+        password: '',
+        passwordConfirm: '',
+        usernameValid: false,
+        passwordValid: false,
+        passwordConfirmValid: false,
+        formValid: false,
+        validationMessages: {
+            username: '',
+            password: '',
+            passwordConfirm: '',
+        },
+        error: null,
+    }
 
     handleSubmit = e => {
         e.preventDefault();
@@ -11,6 +26,72 @@ class RegisterForm extends React.Component {
 
     handleCancel = e => {
         this.props.history.push('/');
+    }
+
+    updateFieldState(value, stateField) {
+        this.setState({ [stateField]: value }, () => {this.validateInputs(value, stateField)})
+    }
+
+    validateInputs(value, stateField) {
+        const newErrors = {...this.state.validationMessages};
+        let hasError = false;
+        const currentVal = value.trim();
+        const VALID_PW_REGEX = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!_@#\$%\^&])[\S]+/;
+
+        // validate for username
+        if (stateField === 'username') {
+            if (currentVal.length === 0) {
+                newErrors.username = 'Username is required';
+                hasError = true;
+            } else {
+                newErrors.username = '';
+                hasError = false;
+            }
+        }
+
+        // validate for password
+        if (stateField === 'password') {
+            if (currentVal.length < 8) {
+                newErrors.password = 'Password must be at least 8 characters';
+                hasError = true;
+            } else if (currentVal.length > 72) {
+                newErrors.password = 'Password must be no more than 72 characters';
+                hasError = true;
+            } else if (currentVal.startsWith(' ') || currentVal.endsWith(' ')) {
+                newErrors.password = 'Password cannot start or end with empty spaces';
+                hasError = true;
+            } else if (!VALID_PW_REGEX.test(currentVal)) {
+                newErrors.password = 'Password must contain at least one each of: upper case, lower case, number, and special character';
+                hasError = true;
+            } else {
+                newErrors.password = '';
+                hasError = false;
+            }
+        }
+
+        // validate for passwordConfirm
+        if (stateField === 'passwordConfirm') {
+            if (currentVal !== this.state.password) {
+                newErrors.passwordConfirm = 'Must match password';
+                hasError = true;
+            } else {
+                newErrors.passwordConfirm = '';
+                hasError = false;
+            }
+        }
+
+        const validKey = stateField + 'Valid';
+        
+        this.setState({
+            validationMessages: newErrors,
+            [validKey]: !hasError,
+        }, this.validateForm);
+    }
+
+    validateForm() {
+        this.setState({
+            formValid: this.state.usernameValid && this.state.passwordValid && this.state.passwordConfirmValid
+        })
     }
 
     render() {
@@ -24,18 +105,42 @@ class RegisterForm extends React.Component {
                     <form className='signup-form' onSubmit={this.handleSubmit}>
                         <div>
                             <label htmlFor="username">Username</label>
-                            <input placeholder='username' type="text" name='username' id='username' required />
+                            <input 
+                                placeholder='username'
+                                type='text'
+                                name='username'
+                                id='username'
+                                onChange={e => this.updateFieldState(e.target.value, 'username')}
+                                required 
+                            />
+                            <span className='validationmessage'>{this.state.validationMessages.username}</span>
                         </div>
                         <div>
                             <label htmlFor="password">Password</label>
-                            <input placeholder='password' type="password" name='password' id='password' required />
+                            <input 
+                                placeholder='password'
+                                type='password'
+                                name='password'
+                                id='password'
+                                onChange={e => this.updateFieldState(e.target.value, 'password')}
+                                required 
+                            />
+                            <span className='validationmessage'>{this.state.validationMessages.password}</span>
                         </div>
                         <div>
                             <label htmlFor="passwordconfirm">Confirm Password</label>
-                            <input placeholder='password' type="password" name='passwordconfirm' id='passwordconfirm' required />
+                            <input 
+                                placeholder='confirm password'
+                                type='password'
+                                name='passwordconfirm'
+                                id='passwordconfirm' 
+                                onChange={e => this.updateFieldState(e.target.value, 'passwordConfirm')}
+                                required 
+                            />
+                            <span className='validationmessage'>{this.state.validationMessages.passwordConfirm}</span>
                         </div>
                         <button type='button' onClick={this.handleCancel}>Cancel</button>
-                        <button type='submit'>Sign Up</button>
+                        <button type='submit' disabled={!this.state.formValid}>Sign Up</button>
                     </form>
                 </section>
             </main>
