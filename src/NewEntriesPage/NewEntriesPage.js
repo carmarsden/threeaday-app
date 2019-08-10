3ce267264a18168d3b606646e9862bfe5a6c3be9
Caroline Marsden <car.marsden@gmail.com>
2019-08-10 08:29:18 -0400

    Control NewEntriesPage form components

--- src/NewEntriesPage/NewEntriesPage.js
+++ src/NewEntriesPage/NewEntriesPage.js
@@ -6,11 +6,59 @@ import NewEntry from '../NewEntry/NewEntry';
 class NewEntriesPage extends React.Component {
     state = {
         totalEntries: 1,
+        entryArray: [],
+    }
+
+    componentDidMount() {
+        const defaultObject = {
+            content: '',
+            public: true,
+            tag_amusement: false,
+            tag_awe: false,
+            tag_contentment: false,
+            tag_gratitude: false,
+            tag_inspiration: false,
+            tag_joy: false,
+            tag_hope: false,
+            tag_love: false,
+            tag_pride: false,
+            tag_serenity: false,
+            tag_other: '',            
+        };
+
+        for (let i = 0; i < this.state.totalEntries; i++) {
+            if (!this.state.entryArray[i]) {
+                const newEntryArray = this.state.entryArray;
+                newEntryArray[i] = defaultObject;
+                this.setState({
+                    entryArray: newEntryArray
+                })
+            }
+        }
     }
 
     addEntry = e => {
+        const defaultObject = {
+            content: '',
+            public: true,
+            tag_amusement: false,
+            tag_awe: false,
+            tag_contentment: false,
+            tag_gratitude: false,
+            tag_inspiration: false,
+            tag_joy: false,
+            tag_hope: false,
+            tag_love: false,
+            tag_pride: false,
+            tag_serenity: false,
+            tag_other: '',            
+        };
+        const newEntryArray = this.state.entryArray;
+        newEntryArray.push(defaultObject);
+
         this.setState({
             totalEntries: this.state.totalEntries + 1,
+            entryArray: newEntryArray,
         })
     }
 
@@ -18,12 +66,31 @@ class NewEntriesPage extends React.Component {
         if (this.state.totalEntries > 1) {
             this.setState({
                 totalEntries: this.state.totalEntries - 1,
+                entryArray: this.state.entryArray.slice(0,-1),
             })    
         }
     }
 
+    handleInputChange = e => {
+        const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+        const name = e.target.name;
+        const idx = e.target.dataset.index;
+
+        const newEntryArray = this.state.entryArray;
+        newEntryArray[idx] = {
+            ...newEntryArray[idx],
+            [name]: value
+        }
+
+        this.setState({
+            entryArray: newEntryArray
+        })
+    }
+
     handleSubmit = e => {
         e.preventDefault();
+        console.log(this.state.entryArray)
+
         window.alert('You submitted the form!');
         this.props.history.push('/mygoodthings')
     }
@@ -35,9 +102,8 @@ class NewEntriesPage extends React.Component {
     render() {
         const entryComponents = [];
         for (let i = 0; i < this.state.totalEntries; i++) {
-            entryComponents.push(<NewEntry key={i} index={i} />)
+            entryComponents.push(<NewEntry key={i} index={i} handleInputChange={this.handleInputChange} />)
         }
-
 
         return (
             <main role='main'>
--- src/NewEntry/NewEntry.js
+++ src/NewEntry/NewEntry.js
@@ -10,27 +10,44 @@ class NewEntry extends React.Component {
             <fieldset className='aboutsection' data-index={idx}>
                 <legend><h3>New Good Thing</h3></legend>
                 <div>
-                    <textarea name='content' id='content' rows='3' cols='50' data-index={idx} required></textarea>
+                    <textarea 
+                        name='content'
+                        id='content'
+                        rows='3'
+                        cols='50'
+                        data-index={idx} 
+                        onChange={e => this.props.handleInputChange(e)}
+                        required 
+                    />
                 </div>
                 <div>
-                    <label><input type="checkbox" name='public' id='public' data-index={idx} defaultChecked /> Share publicly?</label>
+                    <label>
+                        <input 
+                            type="checkbox" 
+                            name='public' 
+                            id='public' 
+                            data-index={idx} 
+                            onChange={e => this.props.handleInputChange(e)}
+                            defaultChecked 
+                        /> Share publicly?
+                    </label>
                     <p className="form-explanation">Public entries are posted "anonymously", not associated with any username</p>
                 </div>
                 <div>
                     <fieldset>
                     <legend>What did it make you feel?</legend>
-                        <label><input type="checkbox" id="tag_amusement" name="tag_amusement" data-index={idx} /> Amusement</label>
-                        <label><input type="checkbox" id="tag_awe" name="tag_awe" data-index={idx} /> Awe</label>
-                        <label><input type="checkbox" id="tag_contentment" name="tag_contentment" data-index={idx} /> Contentment</label>
-                        <label><input type="checkbox" id="tag_gratitude" name="tag_gratitude" data-index={idx} /> Gratitude</label>
-                        <label><input type="checkbox" id="tag_inspiration" name="tag_inspiration" data-index={idx} /> Inspiration</label> 
-                        <label><input type="checkbox" id="tag_joy" name="tag_joy" data-index={idx} /> Joy</label>
-                        <label><input type="checkbox" id="tag_hope" name="tag_hope" data-index={idx} /> Hope</label>
-                        <label><input type="checkbox" id="tag_love" name="tag_love" data-index={idx} /> Love</label>
-                        <label><input type="checkbox" id="tag_pride" name="tag_pride" data-index={idx} /> Pride</label>
-                        <label><input type="checkbox" id="tag_serenity" name="tag_serenity" data-index={idx} /> Serenity</label>
-                        <label><input type="checkbox" id="tag_other" name="tag_other" data-index={idx} /> Other</label>
-                        <input type="text" name="tag_other_text" id="tag_other_text" data-index={idx} />
+                        <label><input type="checkbox" id="tag_amusement" name="tag_amusement" data-index={idx} onChange={e => this.props.handleInputChange(e)} /> Amusement</label>
+                        <label><input type="checkbox" id="tag_awe" name="tag_awe" data-index={idx} onChange={e => this.props.handleInputChange(e)} /> Awe</label>
+                        <label><input type="checkbox" id="tag_contentment" name="tag_contentment" data-index={idx} onChange={e => this.props.handleInputChange(e)} /> Contentment</label>
+                        <label><input type="checkbox" id="tag_gratitude" name="tag_gratitude" data-index={idx} onChange={e => this.props.handleInputChange(e)} /> Gratitude</label>
+                        <label><input type="checkbox" id="tag_inspiration" name="tag_inspiration" data-index={idx} onChange={e => this.props.handleInputChange(e)} /> Inspiration</label> 
+                        <label><input type="checkbox" id="tag_joy" name="tag_joy" data-index={idx} onChange={e => this.props.handleInputChange(e)} /> Joy</label>
+                        <label><input type="checkbox" id="tag_hope" name="tag_hope" data-index={idx} onChange={e => this.props.handleInputChange(e)} /> Hope</label>
+                        <label><input type="checkbox" id="tag_love" name="tag_love" data-index={idx} onChange={e => this.props.handleInputChange(e)} /> Love</label>
+                        <label><input type="checkbox" id="tag_pride" name="tag_pride" data-index={idx} onChange={e => this.props.handleInputChange(e)} /> Pride</label>
+                        <label><input type="checkbox" id="tag_serenity" name="tag_serenity" data-index={idx} onChange={e => this.props.handleInputChange(e)} /> Serenity</label>
+                        <label><input type="checkbox" id="tag_other_check" name="tag_other_check" data-index={idx} onChange={e => this.props.handleInputChange(e)} /> Other</label>
+                        <input type="text" name="tag_other" id="tag_other" data-index={idx} onChange={e => this.props.handleInputChange(e)} />
                     </fieldset>
                 </div>
             </fieldset>
