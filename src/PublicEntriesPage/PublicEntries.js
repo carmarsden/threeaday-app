826243110bb6a317f174033a96ed29975890bb06
Caroline Marsden <car.marsden@gmail.com>
2019-08-09 22:49:21 -0400

    Wire PublicEntries page to api call

--- src/PublicEntriesPage/PublicEntries.js
+++ src/PublicEntriesPage/PublicEntries.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './PublicEntries.css';
 import STORE from '../dummystore';
+import EntriesService from '../services/entries-service';
 
 class PublicEntries extends React.Component {
+    state = {
+        entries: [],
+        error: null,
+    }
+
+    componentDidMount() {
+        EntriesService.getSomePublic(10)
+            .then(res => this.setState({ entries: res }))
+            .catch(res => {
+                if (res.error) {
+                    this.setState({ error: res.error })
+                } else {
+                    this.setState({ error: 'Something went wrong! Please try again later.' })
+                }
+            })
+    }
+
     render() {
-        const entrydisplay = STORE
+        const entrydisplay = this.state.entries
             .filter(entry => entry.public === true)
             .sort(function(a, b) {
                 return new Date(b.date_modified) - new Date(a.date_modified)
