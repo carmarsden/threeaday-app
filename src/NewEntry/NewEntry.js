10de8dd385d710668046d12541db581544af7d58
Caroline Marsden <car.marsden@gmail.com>
2019-08-10 07:45:13 -0400

    Reorganize NewEntry component

--- src/NewEntry/NewEntry.js
+++ src/NewEntry/NewEntry.js
@@ -4,35 +4,36 @@ import './NewEntry.css';
 class NewEntry extends React.Component {
 
     render() {
+        const idx = this.props.index;
 
         return (
-            <section className='aboutsection' data-index={this.props.index}>
+            <fieldset className='aboutsection' data-index={idx}>
+                <legend><h3>New Good Thing</h3></legend>
                 <div>
-                    <label htmlFor="content"><h3>New Good Thing</h3></label>
-                    <textarea name='content' id='content' rows='3' cols='50' required></textarea>
+                    <textarea name='content' id='content' rows='3' cols='50' data-index={idx} required></textarea>
                 </div>
                 <div>
-                    <label><input type="checkbox" name='public' id='public' defaultChecked /> Share publicly?</label>
+                    <label><input type="checkbox" name='public' id='public' data-index={idx} defaultChecked /> Share publicly?</label>
                     <p className="form-explanation">Public entries are posted "anonymously", not associated with any username</p>
                 </div>
                 <div>
                     <fieldset>
                     <legend>What did it make you feel?</legend>
-                        <label><input type="checkbox" id="tag_amusement" name="tag_amusement" /> Amusement</label>
-                        <label><input type="checkbox" id="tag_awe" name="tag_awe" /> Awe</label>
-                        <label><input type="checkbox" id="tag_contentment" name="tag_contentment" /> Contentment</label>
-                        <label><input type="checkbox" id="tag_gratitude" name="tag_gratitude" /> Gratitude</label>
-                        <label><input type="checkbox" id="tag_inspiration" name="tag_inspiration" /> Inspiration</label> 
-                        <label><input type="checkbox" id="tag_joy" name="tag_joy" /> Joy</label>
-                        <label><input type="checkbox" id="tag_hope" name="tag_hope" /> Hope</label>
-                        <label><input type="checkbox" id="tag_love" name="tag_love" /> Love</label>
-                        <label><input type="checkbox" id="tag_pride" name="tag_pride" /> Pride</label>
-                        <label><input type="checkbox" id="tag_serenity" name="tag_serenity" /> Serenity</label>
-                        <label><input type="checkbox" id="tag_other" name="tag_other" /> Other</label>
-                        <input type="text" name="tag_other_text" id="tag_other_text" />
+                        <label><input type="checkbox" id="tag_amusement" name="tag_amusement" data-index={idx} /> Amusement</label>
+                        <label><input type="checkbox" id="tag_awe" name="tag_awe" data-index={idx} /> Awe</label>
+                        <label><input type="checkbox" id="tag_contentment" name="tag_contentment" data-index={idx} /> Contentment</label>
+                        <label><input type="checkbox" id="tag_gratitude" name="tag_gratitude" data-index={idx} /> Gratitude</label>
+                        <label><input type="checkbox" id="tag_inspiration" name="tag_inspiration" data-index={idx} /> Inspiration</label> 
+                        <label><input type="checkbox" id="tag_joy" name="tag_joy" data-index={idx} /> Joy</label>
+                        <label><input type="checkbox" id="tag_hope" name="tag_hope" data-index={idx} /> Hope</label>
+                        <label><input type="checkbox" id="tag_love" name="tag_love" data-index={idx} /> Love</label>
+                        <label><input type="checkbox" id="tag_pride" name="tag_pride" data-index={idx} /> Pride</label>
+                        <label><input type="checkbox" id="tag_serenity" name="tag_serenity" data-index={idx} /> Serenity</label>
+                        <label><input type="checkbox" id="tag_other" name="tag_other" data-index={idx} /> Other</label>
+                        <input type="text" name="tag_other_text" id="tag_other_text" data-index={idx} />
                     </fieldset>
                 </div>
-            </section>
+            </fieldset>
         );
     }
 }
