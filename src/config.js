2456a18bfdbb3ae4dad6b6f911b54e25d48314b7
Caroline Marsden <car.marsden@gmail.com>
2019-08-10 21:10:33 -0400

    Update API variables for environments

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "https://threeaday-server.herokuapp.com/api",
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL,
     TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY || 'threeaday-client-auth-token'
 };
