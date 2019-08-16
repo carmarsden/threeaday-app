c04aac17d4faa84db7515c26cab0c1fcf119fee4
Caroline Marsden <car.marsden@gmail.com>
2019-08-16 15:54:58 -0400

    Add header logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Header.css';
+import vitamin_bottle_logo from '../../images/vitamin_bottle_logo.png';
 
 function Header(props) {
     return (
         <header role='banner' className='bodyheader scrollsnap'>
-            <h1>3aDay</h1>
-            <h2>{props.children}</h2>
+            <img src={vitamin_bottle_logo} alt='Vitamin Bottle Logo' className='header-logo' />
+            <h2 className='header-subheader'>{props.children}</h2>
         </header>
     );    
 }
